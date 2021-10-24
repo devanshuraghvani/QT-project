dbb8fc3480242f0183af018ad80b780beff7f7a1
devanshuraghvani <93083016+devanshuraghvani@users.noreply.github.com>
2021-10-24 18:32:18 +0530

    Add files via upload

---
+++ mainwindow.h
@@ -0,0 +1,38 @@
+#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include <QtCore>
+#include <QtGui>
+#include <QFileDialog>
+#include <QFile>
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private slots:
+    void on_actionNew_triggered();
+
+    void on_actionOpen_triggered();
+
+    void on_actionSave_triggered();
+
+    void on_actionBold_triggered();
+
+    void on_actionSupersceipt_triggered();
+
+    void on_actionSubscript_triggered();
+
+private:
+    Ui::MainWindow *ui;
+    QString mFilename;
+};
+#endif // MAINWINDOW_H
